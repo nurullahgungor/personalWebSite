bdbec9228441fcf6ed12821e326d410044c1a65f
nurullahgungor <ngngrx1@gmail.com>
2024-05-04 18:22:35 +0300

    new style tickets added

--- resources/views/recent-projects.blade.php
+++ resources/views/recent-projects.blade.php
@@ -1,12 +1,16 @@
 <div class="text-white">
-    <div class="flex justify-between max-w-[40em]">
-        <div class"">projects</div>
-        <div>2020-2024</div>
+    <div class="flex justify-between max-w-[40em] py-5">
+        <div class="">
+            <h2>projects</h2>
+        </div>
+        <div>
+            <h2>2020-2024</h2>
+        </div>
     </div>
     <div class="grid grid-cols-1">
-        <div class="w-full">01</div>
-        <div class="w-full">01</div>
-        <div class="w-full">01</div>
-        <div class="w-full">01</div>
+        <div class="w-full border-b-4 py-16 border-indigo-500 hover:px-10 hover:bg-neutral-700 hover:bg-opacity-40 font-audiowide text-3xl font-[700] uppercase">packaging for cyberpulse</div>
+        <div class="w-full border-b-4 py-16 border-indigo-500 hover:px-10 hover:bg-neutral-700 hover:bg-opacity-40 font-audiowide text-3xl font-[700] uppercase">Shopify Store for GlitchyGlow</div>
+        <div class="w-full border-b-4 py-16 border-indigo-500 hover:px-10 hover:bg-neutral-700 hover:bg-opacity-40 font-audiowide text-3xl font-[700] uppercase">03</div>
+
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,10 @@
 <div class="grid grid-cols-2 pb-24">
-    <div class="text-white">
-        WELCOME
+    <div class="text-white text-xl">
+        <h2 class="audiowide">WELCOME</h2>
     </div>
     <div class="text-white">
-    <p class="text-xl" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+    <p class="text-xl tracking-wide leading-loose" 
+    style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
         As a freelance web developer and designer with digital marketing skills, I specialize in creating innovative and exceptional digital experiences for my clients. With a strong work ethic and commitment to delivering results, I work closely with clients to understand their needs and goals, and strive to exceed their expectations with every project.
     </p>
     </div>
