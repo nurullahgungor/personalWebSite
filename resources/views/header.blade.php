a6216c1e8b4fbb055658c452417a6661276ba6b5
nurullahgungor <ngngrx1@gmail.com>
2024-05-04 14:38:29 +0300

    feat : header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,21 @@
-<header class="bg-sky-500">
-    tse
-</header>
+    <div class="w-full flex flex-row justify-between items-center py-5">
+        <div class="basis-1/4">
+            <a href="/">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe2%2Fa9%2F1e%2Fe2a91e256baad2060ba177252f5cc88c.gif&f=1&nofb=1&ipt=15b5d07d2eeb40ab25b93521edafe2e4336f40b20a052a1bb95668ca472b6cb7&ipo=images" alt="logo" class="h-10">
+            </a>
+        </div>
+    
+        <div class="basis-2/4 flex justify-center items-center">
+            <a href="/" class="sm:text-lg text-xl text-white hover:text-gray-700 p-4">HOME</a>
+            <a href="/#projects" class="sm:text-lg text-xl text-white hover:text-gray-700 p-4">PROJECTS</a>
+            <a href="/about" class="sm:text-lg text-xl text-white hover:text-gray-700 p-4">ABOUT</a>
+            <a href="/blog" class="sm:text-lg text-xl text-white hover:text-gray-700 p-4">BLOG</a>
+            <a href="/contact" class="sm:text-lg text-xl text-white hover:text-gray-700 p-4">CONTACT</a>
+        </div>
+
+        <div class="basis-1/4 text-right ">
+            <a href="tel:+905396315967" class=" text-white text-right hover:text-gray-700">
+                +90 539 631 5967
+            </a>
+        </div>
+    </div>
