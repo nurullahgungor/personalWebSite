87481b8980f458313a3c49523146fe9026da2eed
nurullahgungor <ngngrx1@gmail.com>
2024-05-04 14:38:58 +0300

    feat : profile section

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,35 @@
+<section class=" container mx-auto pb-24">
+    <div class="relative w-full mx-auto justify-center items-center">
+
+        <img class="object-cover h-[710px] w-full place-items-center" 
+        src="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fb6_hero.jpg" 
+        alt="here image" 
+        style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" loading="lazy" 
+        sizes="(max-width: 479px) 100vw, (max-width: 767px) 92vw, (max-width: 991px) 94vw, (max-width: 1439px) 90vw, (max-width: 1919px) 81vw, 68vw">
+
+        <div class="z-10 w-full absolute top-0 py-3 bg-black bg-opacity-50 text-white uppercase ">
+        innovative solutions*
+        </div>
+
+        <div class="z-10 grid grid-cols-6 gap-4 absolute inset-0 content-center px-10">
+
+            <div class="col-start-1 col-end-4 text-white content-center">
+                <div class="basis-2/12 m-4"><span class="bg-black bg-opacity-55 p-2">NURULLAH GÜNGÖR<span></div>
+                <div class="basis-8/12 m-4 text-white"><p class="bg-black bg-opacity-55 p-4">I specialize in crafting online spaces that stand the test of time. I utilize innovative design solutions that cater to your unique needs.<p></div>
+                <div class="basis-2/12 m-4 text-black"><button class="bg-white px-4 py-2 text-"> SAY HELLO!</button></div>
+            </div>
+
+            <div class="col-end-7 col-span-2 w-full justify-center items-center bg-black bg-opacity-70 rounded-full">
+                <img class="rounded-full" src="{{ asset('src/img/profile-non-bg.png') }}">
+            </div>
+
+        </div>
+
+    
+
+        <div class="z-10 w-full absolute bottom-0 py-3 bg-black bg-opacity-50 text-white text-base uppercase text-nowrap">
+            impressive design* 
+        </div>
+
+    </div>
+</section>
