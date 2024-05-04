5532d8ab46599d009b9c8fc87940a7d3536db246
nurullahgungor <ngngrx1@gmail.com>
2024-05-04 14:41:41 +0300

    feat : projects section

---
+++ resources/views/recent-projects.blade.php
@@ -0,0 +1,12 @@
+<div class="text-white">
+    <div class="flex justify-between max-w-[40em]">
+        <div class"">projects</div>
+        <div>2020-2024</div>
+    </div>
+    <div class="grid grid-cols-1">
+        <div class="w-full">01</div>
+        <div class="w-full">01</div>
+        <div class="w-full">01</div>
+        <div class="w-full">01</div>
+    </div>
+</div>
