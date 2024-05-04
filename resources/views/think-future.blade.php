b20294ef151e009dd652303b586a0a2a1074049a
nurullahgungor <ngngrx1@gmail.com>
2024-05-04 21:02:42 +0300

    feat : create future banner and make style

---
+++ resources/views/think-future.blade.php
@@ -0,0 +1,36 @@
+    <div class="pb-14">
+        <h2>thinking in the future</h2>
+    </div>
+
+    <div class="w-full">
+    <div data-w-id="e68aa8a9-1352-b0fc-4978-d9c3bf50aee2" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); opacity: 1; transform-style: preserve-3d;" class="services-section-image-container">
+        <img src="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg.jpg" loading="lazy" sizes="(max-width: 479px) 93vw, (max-width: 767px) 92vw, (max-width: 991px) 94vw, (max-width: 1439px) 90vw, (max-width: 1919px) 81vw, 68vw" srcset="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-500.jpg 500w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-800.jpg 800w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-1080.jpg 1080w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-1600.jpg 1600w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-2000.jpg 2000w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg.jpg 2500w" alt="home bg" class="services-background-image">
+        <img src="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg.jpg" loading="lazy" sizes="(max-width: 479px) 93vw, (max-width: 767px) 92vw, (max-width: 991px) 94vw, (max-width: 1439px) 90vw, (max-width: 1919px) 81vw, 68vw" srcset="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-500.jpg 500w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-800.jpg 800w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-1080.jpg 1080w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-1600.jpg 1600w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg-p-2000.jpg 2000w, https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd5_home-bg.jpg 2500w" alt="home bg" class="services-background-image-glow">
+
+        <div style="display: none; transform: translate3d(0px, 100%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" class="animation-image-swipe-reveal-overlay"></div>
+        <div class="noise-overlay"></div>
+    </div>
+
+    <div class="grid grid-cols-11 justify-between mb-20">
+        <!-- TODO: Foreach Column -->
+        <div class="py-20 col-start-1 col-end-6">
+            <div class="font-libre-barcode text-6xl">Branding Design</div>
+            <span class="">Our branding design service helps establish a unique identity for your business, building recognition and trust with your audience.</span>
+        </div>
+        <div class="py-20 col-start-7 col-end-12">
+            <div class="font-libre-barcode text-6xl">Branding Design</div>
+            <span class="">Our branding design service helps establish a unique identity for your business, building recognition and trust with your audience.</span>
+        </div>
+        <div class="py-20 col-start-1 col-end-6">
+            <div class="font-libre-barcode text-6xl">Branding Design</div>
+            <span class="">Our branding design service helps establish a unique identity for your business, building recognition and trust with your audience.</span>
+        </div>
+        <div class="py-20 col-start-7 col-end-12">
+            <div class="font-libre-barcode text-6xl">Branding Design</div>
+            <span class="">Our branding design service helps establish a unique identity for your business, building recognition and trust with your audience.</span>
+        </div>
+        <div class="flex col-start-11 justify-between">
+            <h2>about<h2>
+            <h2>></h2>
+        </div>
+    </div>
