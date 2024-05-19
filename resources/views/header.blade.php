65dbe7ddf7e20d6fb7c2da1f882cd9fd0711d7bf
nurullahgungor <ngngrx1@gmail.com>
2024-05-19 05:20:03 +0300

    arr : change navbar and build file

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,7 +4,7 @@
                 <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe2%2Fa9%2F1e%2Fe2a91e256baad2060ba177252f5cc88c.gif&f=1&nofb=1&ipt=15b5d07d2eeb40ab25b93521edafe2e4336f40b20a052a1bb95668ca472b6cb7&ipo=images" alt="logo" class="h-10">
             </a>
         </div>
-    
+
         <div class="basis-2/4 flex justify-center items-center">
             <a href="/" class="sm:text-lg text-xl text-white hover:text-gray-700 p-4">HOME</a>
             <a href="/#projects" class="sm:text-lg text-xl text-white hover:text-gray-700 p-4">PROJECTS</a>
@@ -14,8 +14,8 @@
         </div>
 
         <div class="basis-1/4 text-right ">
-            <a href="tel:+905396315967" class=" text-white text-right hover:text-gray-700">
-                +90 539 631 5967
+            <a href="https://github.com/nurullahgungor/" class=" text-white text-right hover:text-gray-700">
+                nurullahgungor
             </a>
         </div>
     </div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,15 +1,15 @@
 <section class="container mx-auto pb-24">
     <div class="relative w-full mx-auto justify-center items-center border-y-4 border-indigo-200 border-y-indigo-500">
 
-        <img class="object-cover h-[710px] w-full place-items-center" 
-        src="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fb6_hero.jpg" 
-        alt="here image" 
-        style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" loading="lazy" 
+        <img class="object-cover h-[710px] w-full place-items-center"
+        src="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fb6_hero.jpg"
+        alt="here image"
+        style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;" loading="lazy"
         sizes="(max-width: 479px) 100vw, (max-width: 767px) 92vw, (max-width: 991px) 94vw, (max-width: 1439px) 90vw, (max-width: 1919px) 81vw, 68vw">
 
-        
+
         <div class="z-10 grid grid-cols-6 gap-4 absolute inset-0 content-center px-10">
-            
+
             <div class="col-start-1 col-end-4 text-white content-center">
                 <div class="basis-2/12 my-10 ">
                     <span style="font-family: Audiowide; sans-serif; font-weight: 400; font-style: bold;" class="bg-black bg-opacity-70 px-6 py-3 text-4xl">NURULLAH GÜNGÖR<span>
@@ -23,14 +23,14 @@
                 </div>
 
             </div>
-            
+
             <div class="col-end-7 col-span-2 w-full justify-center items-center bg-black bg-opacity-70 rounded-full">
-                <img class="rounded-full" src="{{ asset('src/img/profile-non-bg.png') }}">
+                <img class="rounded-full" src="{{ asset('img/profile-photo.png') }}">
             </div>
-            
+
         </div>
-        
-        
+
+
         <div class="z-10 w-full absolute top-0 py-3 bg-black bg-opacity-50 text-white uppercase ">innovative solutions*</div>
 
         <div class="z-10 w-full absolute bottom-0 py-3 bg-black bg-opacity-50 text-white text-base uppercase text-nowrap">impressive design*</div>
